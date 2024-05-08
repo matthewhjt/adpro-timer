9ec7d624bcef59119a360388783cc8da779589ac
Matthew <matthewhjt72@gmail.com>
2024-05-08 18:47:44 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-1.2 
+## 1.2 
 
 ![](1.2.png)
 
 "hey hey" was printed first because "howdy!" and "done!" is inside an async function. It waits for the timer future for 2 seconds before printing the other lines.
+
+## 1.3
+![alt text](1.3.png)
+
+This time there are multiple spawners. It spawns asynchrounously that's why all the 'howdy'-ies are spawned first before all the 'done'-s (there's a sleep for about 2 seconds after each of the 'howdy'-ies are printed). In the end the spawner wasn't dropped that's why the program doesn't finish.
--- src/main.rs
+++ src/main.rs
@@ -106,11 +106,25 @@ fn main() {
         println!("Matt's Komputer: done!");
     });
 
+    spawner.spawn(async {
+        println!("Matt's Komputer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Matt's Komputer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Matt's Komputer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Matt's Komputer: done3!");
+    });
+
     println!("Matt's Komputer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
