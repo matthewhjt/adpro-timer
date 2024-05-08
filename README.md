dae22dd3ab73003afcab4778feed8736ebc51cab
Matthew <matthewhjt72@gmail.com>
2024-05-08 18:42:14 +0700

    Experiment 1.2: Understanding how it works.

---
+++ README.md
@@ -0,0 +1,5 @@
+1.2 
+
+![](1.2.png)
+
+"hey hey" was printed first because "howdy!" and "done!" is inside an async function. It waits for the timer future for 2 seconds before printing the other lines.
--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,8 @@ fn main() {
         println!("Matt's Komputer: done!");
     });
 
+    println!("Matt's Komputer: hey hey");
+
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
